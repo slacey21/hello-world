3083f2401d9ce924e96f2d4532b6988b7d136ae6
Sean Lacey <seanlacey2164@gmail.com>
2024-08-11 20:10:03 -0400

    Implement view helpers and variables to simplify views

--- hello.js
+++ hello.js
@@ -8,24 +8,40 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 app.use(morgan("common"));
 
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? "current" : "";
+}
+
 app.get("/", (req, res) => {
   res.redirect("/english");
 });
 
 app.get("/english", (req, res) => {
-  res.render("hello-world-english");
+  res.render("hello-world-english", {
+    currentPath: req.path,
+    language: "en-US",
+  });
 });
 
 app.get("/french", (req, res) => {
-  res.render("hello-world-french");
+  res.render("hello-world-french", {
+    currentPath: req.path,
+    language: "fr-FR",
+  });
 });
 
 app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian");
+  res.render("hello-world-serbian", {
+    currentPath: req.path,
+    language: "sr-Cyrl-rs",
+  });
 });
 
 app.get("/spanish", (req, res) => {
-  res.render("hello-world-spanish");
+  res.render("hello-world-spanish", {
+    currentPath: req.path,
+    language: "es-ES",
+  });
 });
 
 app.listen(3000, "localhost", () => {
